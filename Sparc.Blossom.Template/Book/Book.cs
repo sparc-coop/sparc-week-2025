7a76e216d89bde1a49baba579cf6d470e35e5eab
Antonio Correia <antonio.alves.correia@gmail.com>
2025-01-06 21:19:19 -0300

    creating basic CRUD to get familiar with Blossom concepts

---
+++ Sparc.Blossom.Template/Book/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace Sparc.Blossom.Template.Books;
+
+public class Book : BlossomEntity<string>
+{
+    public string Title { get; set; }
+    public string? Description { get; set; }
+
+    public Book(string title, string description) : base(Guid.NewGuid().ToString())
+    {
+        Title = title;
+        Description = description;
+    }
+}
---
+++ Sparc.Blossom.Template/Book/Books.cs
@@ -0,0 +1,10 @@
+﻿namespace Sparc.Blossom.Template.Books;
+
+public class Books(BlossomAggregateOptions<Book> options) : BlossomAggregate<Book>(options)
+{
+    public BlossomQuery<Book> ByTitle(string title)
+        => Query().Where(x => x.Title == title);
+
+    public BlossomQuery<Book> Paginated(int page, int perPage)
+        => Query().SkipTake(page*perPage, perPage);
+}

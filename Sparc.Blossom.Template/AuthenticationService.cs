6e482ad1e121fa560478b7a1d8418d5d6dd1f020
manishk85 <manishkavi145@gmail.com>
2025-01-10 21:50:12 +0530

    Initial commit for authentications

---
+++ Sparc.Blossom.Template/AuthenticationService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SparcWeek2025.Services
+{
+    public class AuthenticationService
+    {
+        private readonly NavigationManager _navigationManager;
+
+        public AuthenticationService(NavigationManager navigationManager)
+        {
+            _navigationManager = navigationManager;
+        }
+
+        public async Task<bool> LoginAsync(string username, string password)
+        {
+            // Mock login logic
+            if (username == "admin" && password == "password")
+            {
+                // Redirect to dashboard after successful login
+                _navigationManager.NavigateTo("/dashboard");
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<bool> RegisterAsync(string username, string password)
+        {
+            return true;
+        }
+
+        public async Task LogoutAsync()
+        {
+            // Mock logout logic
+            _navigationManager.NavigateTo("/login");
+        }
+    }
+}
+
--- Sparc.Blossom.Template/Program.cs
+++ Sparc.Blossom.Template/Program.cs
@@ -1,12 +1,12 @@
 using Sparc.Blossom.Template;
 
 var builder = BlossomApplication.CreateBuilder(args);
-
+builder.Services.AddScoped<Microsoft.AspNetCore.Authentication.AuthenticationService>();
 var app = builder.Build();
 
 // Add some random forecast data to the Blossom database (in-memory is built in, no need to hook up a DB to build your app!)
-using var scope = app.Services.CreateScope();
-var forecastRepository = scope.ServiceProvider.GetRequiredService<IRepository<Forecast>>();
-await forecastRepository.AddAsync(Forecast.Generate(60));
+//using var scope = app.Services.CreateScope();
+//var forecastRepository = scope.ServiceProvider.GetRequiredService<IRepository<Forecast>>();
+//await forecastRepository.AddAsync(Forecast.Generate(60));
 
 await app.RunAsync<Html>();

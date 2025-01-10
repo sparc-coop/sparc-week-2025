78d6a536aadf6ca139ac0019d8f6ab40f8d12432
Antonio Correia <antonio.alves.correia@gmail.com>
2025-01-10 19:14:33 -0300

    Adding entities constructors

--- Sparc.Blossom.Template/SolarCalculator/SolarEstimate.cs
+++ Sparc.Blossom.Template/SolarCalculator/SolarEstimate.cs
@@ -6,4 +6,12 @@ public class SolarEstimate : BlossomEntity<string>
     public string Address { get; set; }
     public string Type { get; set; }
     public double AvgMonthlyEletricityBill { get; set; }
+
+    public SolarEstimate(string email, string address, string type, double avgMonthlyElectricityBill)
+    {
+        Email = email;
+        Address = address;
+        Type = type;
+        AvgMonthlyEletricityBill = avgMonthlyElectricityBill;
+    }
 }
--- Sparc.Blossom.Template/SolarPlant/SolarPlant.cs
+++ Sparc.Blossom.Template/SolarPlant/SolarPlant.cs
@@ -6,5 +6,14 @@ public class SolarPlant : BlossomEntity<string>
     public string Location { get; set; }   
     public double CapacityMW { get; set; } 
     public DateTime CommissionedDate { get; set; }
-    public string Owner { get; set; }      
+    public string Owner { get; set; }
+
+    public SolarPlant(string name, string location, double capacityMW, DateTime commissionedDate, string owner)
+    {
+        Name = name;
+        Location = location;
+        CapacityMW = capacityMW;
+        CommissionedDate = commissionedDate;
+        Owner = owner;
+    }
 }
